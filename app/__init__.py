0da1331c7e050925612b3aad16be8cc594afc456
Faris Alzahrani <falofoc@gmail.com>
2025-04-04 12:28:17 +0300

    Disable Supabase and remove Supabase packages for clean deployment

--- app/__init__.py
+++ app/__init__.py
@@ -73,7 +73,7 @@ login_manager.login_message = 'Please log in to access this page.'
 login_manager.session_protection = "strong"
 
 # Initialize Supabase client - USING A FLAG TO MAKE IT OPTIONAL
-ENABLE_SUPABASE = True
+ENABLE_SUPABASE = False
 
 try:
     if ENABLE_SUPABASE:
